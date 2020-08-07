71716a8362693fc4ecd618467e4ccc987ba8fdfc
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-08-06 23:56:28 -0400

    Attempting to get rid of any reference to the gatsby icon so that I can deploy to Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lend A Note`,
+    description: `A sheet music lending library for music teachers across the country to share their notes so we can all make beautiful music together.`,
+    author: `Leah Mattern`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
